1119bb3d7d7e9ee7dcc280471e6e32c2f3f2da84
Mind Scratching <huangtnhao@icloud.com>
2020-11-28 10:12:30 +0800

    Style Fix

--- python/src/navigation.js
+++ python/src/navigation.js
@@ -1,6 +1,6 @@
 document.getElementById('nav').innerHTML = `
 <a class="logo" href="../index.html"><img src="../resource/mind-scratching-square.png" class="nav"></a>
 <a class="title" href="index.html">Home Page</a>
-<a class="title" href="challenge/index.html">Challenge</a>
+<a class="title" href="challenge/index.html">Challenges</a>
 <a class="title" href="disclaimer.html">Disclaimer</a>
 `
--- scratch/src/navigation.js
+++ scratch/src/navigation.js
@@ -1,7 +1,7 @@
 document.getElementById('nav').innerHTML = `
 <a class="logo" href="../index.html"><img src="../resource/mind-scratching-square.png" class="nav"></a>
 <a class="title" href="index.html">Home Page</a>
-<a class="title" href="tutorial/index.html">Tutorial</a>
+<a class="title" href="tutorial/index.html">Tutorials</a>
 <a class="nav" href="tutorial/dictionary.html">Dictionary</a>
 <a class="title" href="disclaimer.html">Disclaimer</a>
 `
